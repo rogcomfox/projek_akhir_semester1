f4ecefc6248be8bbcdbd750ce87b9c2972574495
ygladiensyah <ygladiensyah@gmail.com>
2018-11-23 18:18:48 +0700

    second commit

---
+++ src/com/nusantarian/developer/Game.java
@@ -0,0 +1,11 @@
+package com.nusantarian.developer;
+
+import java.util.*;
+
+public class Game {
+    private static Scanner in = new Scanner(System.in);
+    private static int i;
+    private static void showmenu(){
+
+    }
+}
