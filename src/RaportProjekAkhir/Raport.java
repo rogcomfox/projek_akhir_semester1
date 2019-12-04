e8d93d020f6e8ec956d4d0c50441c4747cbef968
rogcomfox <ygladiensyah@gmail.com>
2019-12-05 02:44:39 +0700

    add raport

---
+++ src/RaportProjekAkhir/Raport.java
@@ -0,0 +1,10 @@
+package RaportProjekAkhir;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Raport {
+    public static void main(String[] args) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
